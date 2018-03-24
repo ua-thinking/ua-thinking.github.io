fa82a80a0df2dfa66ea113c25153541e24154abf
Junior <darkness.furacao2@gmail.com>
2018-03-23 21:11:57 -0300

    test

--- Starbound/disable-button.js
+++ Starbound/disable-button.js
@@ -1,27 +1,21 @@
-// function to disable and enable buttons, receives an array with button IDs
-// from http://coursesweb.net/javascript/
-function disableEnableBtn(ids) {
-  // traverses the array with IDs
-  var nrids = ids.length;
-  for(var i=0; i<nrids; i++) {
-    // registers onclick event to each button
-    if(document.getElementById(ids[i])) {
-      document.getElementById(ids[i]).onclick = function() {
-        this.setAttribute('disabled', 'disabled');     // disables the button by adding the 'disabled' attribute
-        this.innerHTML = 'Desativado';        // changes the button text
-        var idbtn = this.id;       // stores the button ID
+var sec = 70;
+var myTimer = document.getElementById('myTimer');
+var commitButton = document.getElementById('commitButton');
+window.onload = countDown;
 
-        // calls a function after 2 sec. (2000 milliseconds)
-        setTimeout( function() {
-          document.getElementById(idbtn).removeAttribute('disabled');         // removes the "disabled" attribute
-          document.getElementById(idbtn).innerHTML = 'Desativado';        // changes tne button text
-        }, 70000 );
-      }
-    }
+function countDown() {
+  if (sec < 10) {
+    myTimer.innerHTML = "0" + sec;
+  } else {
+    myTimer.innerHTML = sec;
   }
+  if (sec <= 0) {
+    $("#commitButton").removeAttr("disabled");
+    $("#commitButton").removeClass().addClass("btnEnable");
+    $("#myTimer").fadeTo(2500, 0);
+    commitButton.innerHTML = "Salvar";
+    return;
+  }
+  sec -= 1;
+  window.setTimeout(countDown, 1000);
 }
-
-// array with IDs of buttons
-var btnid = ['commitButton', 'perform-commit'];
-
-disableEnableBtn(btnid);      // calls the function
