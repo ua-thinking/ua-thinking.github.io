3923e2da033b74679ca73a557148ab4ccebcd6a8
JuniorDark <darkness_furacao@hotmail.com>
2019-01-10 05:58:47 -0200

    Add Starbound_Itens

---
+++ Starbound_Itens/editor_api.js
@@ -0,0 +1,418 @@
+"use strict";
+
+const editors_per_page = 25
+const schema = {
+  schema : {
+    options: {
+        collapsed: true,
+    },
+    type: "object",
+    format: "table",
+    properties: {
+      "Comment":{
+        type: "string",
+        title: "Comentário"
+      },
+      "DeniedAlternatives":{
+        type: "array",
+        format: "table",
+        title: "Traduções descartadas",
+        options:{
+          disable_array_add: false,
+          disable_array_delete: false,
+          collapsed: true
+        },
+        items: {
+          title: "Opção",
+          format: "text",
+          type: "string",
+        }
+      },
+      "Files":{
+        type: "object",
+        title: "Usado em:",
+        format: "table",
+        options:{
+          collapsed: true,
+          disable_array_add: true,
+          disable_array_delete: true,
+        },
+        patternProperties:{
+          "^.+$":{
+            type: "array",
+            options:{
+              collapsed: true,
+              disable_array_add: true,
+              disable_array_delete: true,
+            },
+            items:{
+              options:{
+                collapsed: true,
+                disable_array_add: true,
+                disable_array_delete: true,
+              },
+              type: "string",
+              title: "Caminho dentro do json",
+              format: "url"
+            }
+          }
+        }
+      },
+      "Texts":{
+        title: "Textos",
+        options:{
+          disable_collapse: true,
+          required: true
+        },
+        type: "object",
+        properties: {
+          "Eng":{
+            type: "string",
+            title: "Texto em inglês",
+            format: "textarea",
+          },
+          "Por": {
+            type: "string",
+            title: "Texto em português",
+            format: "textarea",
+            options:{
+              required: true
+            },
+          }
+        },
+        default_properties: ["Por", "Eng"]
+      },//Texts
+    }
+  }
+}
+
+function check_label_length(text, after_check, maxwidth, maxheight)
+{
+  let width = maxwidth
+  let height = maxheight - 1 // first string taken anyway
+  let splited = text.split(/([^\t\s\n\r]+|\r?\n)/)
+  for (let s in splited)
+  {
+    if (splited[s].length == 0 || splited[s].match(/\^.+;/))
+      continue
+    else if(splited[s] == '\n')
+    {
+      height--
+      width = maxwidth
+    }
+    else
+      width -= splited[s].length
+    if (width < 0 && splited[s] != ' ')
+    {
+      width = maxwidth - splited[s].length
+      height--
+    }
+  }
+  after_check(-height)
+}
+
+function theEditor(holder, navigator)
+{
+  this.holder = holder
+  this.navbar = navigator
+  this.filedata = {}
+  this.json = {}
+  this.subeditors = {}
+  this.touched = false
+}
+
+let onImage = function(imgUrl){
+  let image = document.createElement('IMG')
+  image.src = imgUrl
+  image.className = 'img-rounded img-responsive'
+  return image
+}
+
+function makeCarousel(name)
+{
+  let carousel = document.createElement('div')
+  carousel.id = "Carousel-" + name
+  carousel.className = "carousel slide"
+  $(carousel).attr("data-ride", "carousel")
+  let left = document.createElement('a')
+  let right = document.createElement('a')
+  $(left).add(right).addClass("carousel-control")
+  $(left).addClass("left")
+  $(right).addClass("right")
+  left.href = '#Carousel-' + name
+  right.href = '#Carousel-' + name
+  $(left).attr("data-slide", "prev")
+  $(right).attr("data-slide", "next")
+  $(left).append("<span class='glyphicon glyphicon-chevron-left'></span>")
+  $(right).append("<span class='glyphicon glyphicon-chevron-right'></span>")
+  $(carousel).append(left, right)
+  return carousel
+}
+
+function referenceLookup(subtree, path)
+{
+  let pageref = path.split('/').pop().split('.')[0]
+  let siteUrl = "https://starbounder.org"
+  let apiUrl = siteUrl + "/mediawiki/api.php?"
+  let to_replace = $(subtree).find('h3')
+  let sysAnch = document.createElement("A")
+  let container = document.createElement("DIV")
+  sysAnch.href = siteUrl + "/Data:" + pageref
+  sysAnch.target = "_blank"
+  sysAnch.innerHTML = path
+  container.appendChild(sysAnch)
+  let carousel = makeCarousel(path.replace(/[\/\.]/g, '_'))
+  let imageLink = document.createElement('A')
+  imageLink.className = 'carousel-inner'
+  imageLink.target = "_blank"
+  $(carousel).prepend(imageLink)
+  to_replace.replaceWith(container)
+  let imagesResponse = function(response){
+    let icon_needed = true
+    let pngneeded = true
+    if(response.query.allimages.length > 0)
+      container.appendChild(carousel)
+    for(let i in response.query.allimages){
+      let img = response.query.allimages[i]
+      if(icon_needed && img.name.match(/Icon\.png/)){
+        $(container).parent().parent().parent().parent().parent()
+          .siblings('h3').append("<img class=btn-group src='" + img.url + "'>")
+        icon_needed = false
+      }
+      else if(pngneeded || img.name.match(/Sample/) ||
+              ! img.name.match(/png$/)){
+        $(imageLink).append("<img class='item center-block img-responsive" +
+          " img-rounded' src='"+ img.url + "'>")
+        pngneeded = false
+      }
+    }
+    $(imageLink).children().first().addClass("active")
+  }
+  let serviceResponse = function(response){
+    let name = ""
+    if(response.error){
+      name = pageref.replace(/\s/g, "_")
+    }
+    else{
+      for (let i in response.parse.links){
+        let link = response.parse.links[i]
+        if(link.ns == 0){
+          name = encodeURIComponent(link["*"].replace(/\s/g, '_'))
+          break
+        }
+      }
+      if(name == "") return
+    }
+    imageLink.href = siteUrl + "/" + name
+    let wpurl = apiUrl + $.param({
+      action:"query",
+      format:"json",
+      aifrom:name,
+      list:"allimages",
+      aiprefix:name,
+    })
+    $.ajax(wpurl, {dataType:"jsonp"}).done(imagesResponse)
+
+  }
+  let starbounderUrl = apiUrl + $.param({
+      action: "parse",
+      format: "json",
+      prop: "links",
+      page: "Data:" + pageref,
+  })
+  $.ajax(starbounderUrl,{dataType: "jsonp"})
+    .done(serviceResponse)
+}
+
+theEditor.prototype.generate_label_checker = function(ii, maxwidth, maxheight)
+{
+  let ed = this
+  function after_check(diff)
+  {
+    $(ed.subeditors[ii].getEditor('root.Texts.Por').container)
+      .find('#overflow-warning').remove()
+    $(ed.subeditors[ii].root_container).find('#overflow-head').remove()
+    if (diff > 0)
+    {
+      $(ed.subeditors[ii].root_container).addClass('alert-danger')
+      $(ed.subeditors[ii].getEditor('root.Texts.Por').container)
+        .append('<p id="overflow-warning">O texto é mais longo do que a janela!' +
+        ' Linhas extras: '+ diff +'!</p>')
+      $(ed.subeditors[ii].root_container).children('h3')
+      .append('<span id="overflow-head">O texto é muito longo!</span>')
+    }
+    else
+    {
+      $(ed.subeditors[ii].root_container).removeClass('alert-danger')
+    }
+  }
+  return function(first)
+  {
+    let curval = ed.subeditors[ii].getEditor('root.Texts.Por').getValue()
+    check_label_length(curval, after_check, maxwidth, maxheight)
+    ed.touched = true
+    if (first){ ed.touched = false}
+  }
+}
+
+
+theEditor.prototype.load_part = function (start, selected)
+{
+  this.subeditors = {}
+  this.holder.html("")
+  var ed = this
+  var to_highlight = []
+  for (var i=start;i<this.json.length && i<start+editors_per_page;i+=1)
+  {
+    var fixed_schema = schema
+    var titletext = this.json[i]['Texts']['Eng']
+    if (! ('Por' in this.json[i]['Texts']))
+    {
+      this.json[i]['Texts']['Por'] = ""
+    }
+    if (titletext.length > 20)
+    {
+      titletext = titletext.slice(0, 20) + "..."
+    }
+    if (this.json.length-start < 3)
+    {
+      fixed_schema.schema.options.collapsed = false
+    }
+    fixed_schema.schema.title = titletext
+    var subeditor = new JSONEditor(this.holder[0], fixed_schema)
+    subeditor.setValue(this.json[i])
+    subeditor.root_container.id = 'element-' + i
+    if (i === selected)
+      $(subeditor.root_container).addClass('alert-warning')
+    this.subeditors[i] = subeditor
+    if (this.json[i]['DeniedAlternatives'] &&
+      this.json[i]['DeniedAlternatives'].length > 0)
+    {
+      $(subeditor.root_container)
+        .find('div[data-schemapath="root.DeniedAlternatives"]')
+        .addClass('alert-success')
+    }
+    let ccheck = ed.generate_label_checker(i, 40, 17)
+    ccheck(true)
+    subeditor.watch('root.Texts.Por', ccheck)
+    if (this.json[i]['Texts']['Por'] === "")
+    {
+      subeditor.root_container.className += ' alert-info'
+    }
+  }
+  $(this.holder).find("[name$=\"[Eng]\"]").each(function(i,d){d.readOnly=true})
+  $(this.holder).find("[name$=\"[Comment]\"]").each(function(i,d){d.readOnly=true})
+  $(this.holder).find("textarea[name$=\"]\"]").each(function(i,d){
+      d.className = 'input-lg form-control'
+      if (d.value.length != 0)
+      {
+        d.rows = Math.ceil(d.value.length/27)
+      }
+    })
+  $(this.holder).find('div[data-schemapath^="root.Files."]').each(function(i,c){
+      let prefix_len = "root.Files.".length
+      let path = $(c).attr('data-schemapath').substring(prefix_len)
+      $(c).find('button').hide()
+      $(c).find('[data-schemapath^="root.Files.' + path + '."]').each(function(i,p)
+      {
+        let internal_path = $(p).find('input')[0].value
+        $(p).parent().append('<p>' + internal_path + '</p>')
+        $(p).hide()
+      })
+      referenceLookup(c, path)
+    })
+  $('table.table-bordered').css('width', '')
+}
+
+theEditor.prototype.get_json = function ()
+{
+  var thejson = this.json
+  $.each(this.subeditors, function(i, s)
+  {
+    thejson[i] = s.getValue()
+  })
+  return thejson
+}
+
+theEditor.prototype.json_onload = function (data, gotopattern)
+{
+  this.json = []
+  this.navbar.innerHTML = ""
+  let page = 1
+  $('#current-filename').text(this.filedata.name)
+  let editor = this
+  function get_onclick(start)
+  {
+    return (function(){
+      $.each(editor.subeditors, function(n, e){
+        editor.json[n] = e.getValue()
+        e.destroy()
+      })
+      $(editor.navbar).children().removeClass('active')
+      $(editor.navbar).find('#navbarel-' + start).addClass('active')
+      editor.load_part(start)
+    })
+  }
+  let has_untranslated = false
+  let ii = 0
+  let target_pagestart = 0
+  let target_i = -1
+  let last_i = 0
+  for (let i = 0 ; i < data.length; i += 1)
+  {
+    let texts = data[i]["Texts"]
+    if (gotopattern && (texts["Eng"].match(gotopattern) ||
+        (texts["Por"] && texts["Por"].match(gotopattern)))){
+      target_pagestart = last_i
+      target_i = i
+    }
+    let translated = ("Por" in texts) && (texts["Por"].length > 0)
+    has_untranslated = has_untranslated || !translated
+    if (ii == editors_per_page - 1 | i == data.length - 1)
+    {
+      let navelement = document.createElement('li')
+      navelement.id = 'navbarel-' + last_i
+      let navbutton = document.createElement('a')
+      navbutton.innerHTML = page
+      navbutton.title = page
+      navbutton.onclick = get_onclick(last_i)
+      if (has_untranslated)
+      {
+        $(navelement).addClass("untranslated")
+      }
+      navelement.appendChild(navbutton)
+      this.navbar.append(navelement)
+      page += 1
+      last_i = i+1
+      ii = -1 //not 0 due to increment after
+      has_untranslated = false
+    }
+    ii += 1
+  }
+  this.json = data
+  $('#navbarel-' + target_pagestart).addClass('active')
+  this.load_part(target_pagestart, target_i)
+  if (target_i >=0){
+    window.location.hash = '#element-' + target_i
+  }
+}
+
+
+theEditor.prototype.reset = function ()
+{
+  this.navbar.html("")
+  this.holder.html("")
+  $('#current-filename').text('')
+  $.each(this.subeditors, function(n, e){
+      e.destroy()
+    })
+  this.subeditors = {}
+  this.json = {}
+  this.filedata = {}
+}
+
+theEditor.prototype.open_json = function (content, gotopattern)
+{
+  let data = $.parseJSON(content)
+  this.json_onload(data, gotopattern)
+}
---
+++ Starbound_Itens/githubapi.js
@@ -0,0 +1,235 @@
+"use strict";
+const api_prefix = "https://api.github.com/"
+
+function setAuthData(xhr, authdata)
+{
+  if (authdata.uname && authdata.upass)
+  {
+    xhr.setRequestHeader ("Authorization", "Basic " +
+      btoa(authdata.uname + ":" + authdata.upass));
+  }
+}
+
+function GHAccount(authdata, owner, repo, branch, holder)
+{
+  this.authdata = authdata
+  this.owner = owner
+  if (holder) {
+    this.targetholder = holder
+  }
+  else {
+    this.targetholder = owner
+  }
+  this.repo = repo
+  this.branch = branch
+}
+
+GHAccount.prototype.rawRequest = function(url, type, data, onprogress)
+{
+  let acc = this
+  let result = new Promise( function(ok, fail)
+  {
+    let ajax = new XMLHttpRequest()
+    ajax.open(type, api_prefix + url, true)
+    ajax.onerror = function(stat) {fail(ajax, stat, "erro de conexão")}
+    if (typeof onprogress == "function")
+      ajax.onprogress = function(pe)
+      {
+        if (pe.lengthComputable)
+          onprogress(pe.loaded/pe.total)
+        else
+          onprogress(1)
+      }
+    ajax.onload = function () {
+      if (ajax.status >= 400)
+        fail(ajax, ajax.status, ajax.response)
+      else
+        ok(ajax.response);
+    };
+    setAuthData(ajax, acc.authdata)
+    ajax.send(data);
+  })
+  return result
+}
+GHAccount.prototype.request = function(url, type, data, onprogress)
+{
+  let result = this.rawRequest(url, type, data, onprogress)
+  return result.then(function(answer)
+    {if (answer != "") return JSON.parse(answer)})
+}
+
+GHAccount.prototype.check_authdata = function (authdata, on_success, on_fail)
+{
+  var acc = this
+  acc.authdata = authdata
+  let authed = acc.request("user", "GET", {})
+  authed.then(function(a)
+  {
+    acc.authdata.uname = a.login;
+    on_success(a)
+  }).catch(function(a)
+  {
+    acc.authdata = {}
+    on_fail(a)
+  })
+}
+
+GHAccount.prototype.get_repo_suffix = function()
+{
+  return "repos/" + this.targetholder + '/' + this.repo + '/'
+}
+
+GHAccount.prototype.ifNotAMember = function ()
+{
+  let acc = this
+  let url = "orgs/" + "StarboundPTBR" + "/members/" + acc.authdata.uname
+  return new Promise(function(ok, fail) {
+    acc.rawRequest(url, "GET", {}).catch(ok).then(function(){})
+  })
+}
+
+GHAccount.prototype.ifHasFork = function()
+{
+  let acc = this
+  let url = "repos/" + acc.authdata.uname + '/' + acc.repo
+  return new Promise(function(ok, fail){
+    console.log("in promise")
+    acc.request(url, "GET", {}).then(function(a){
+      console.log("in promise resolve")
+      console.log(a)
+      if (a.parent.full_name == acc.owner + '/' + acc.repo)
+      {ok(a)}
+      else{fail(a)}
+    }).catch(fail)
+  })
+}
+
+GHAccount.prototype.switchToFork = function()
+{
+  this.targetholder = this.authdata.uname
+}
+
+GHAccount.prototype.switchToMain = function()
+{
+  this.targetholder = this.owner
+}
+
+GHAccount.prototype.mergeFrom = function (head)
+{
+  let acc = this
+  let url = acc.get_repo_suffix() + 'merges'
+  let head_json_promise = acc.getHead()
+  let data = {base: acc.branch, head: head}
+  let result = acc.request(url, "POST", JSON.stringify(data))
+  let new_head = result.then(function(commit){
+    return acc.updateHead(commit.sha)
+  })
+  return Promise.resolve(new_head)
+}
+
+GHAccount.prototype.getHead = function(on_progress)
+{
+  return this.getJSON(this.get_repo_suffix() + "git/refs/heads/" +
+    this.branch, {}, on_progress)
+}
+
+GHAccount.prototype.updateHead = function(sha, on_progress)
+{
+  this.request(this.get_repo_suffix() + "git/refs/heads/" +
+    this.branch, "PATCH", JSON.stringify({sha:sha}), on_progress)
+
+}
+
+GHAccount.prototype.compareToOwner = function ()
+{
+  let acc = this
+  let url = acc.get_repo_suffix() + 'compare/' + acc.owner +
+    ":" + acc.branch + "..." + acc.authdata.uname + ":" + acc.branch
+  return acc.request(url, "GET", {})
+}
+
+GHAccount.prototype.fork = function()
+{
+  let acc = this
+  let url = acc.get_repo_suffix() + "forks"
+  return acc.request(url, "POST", [])
+}
+
+GHAccount.prototype.getJSON = function (url, data, onprogress)
+{
+  return this.request(url, "GET", data, onprogress)
+}
+
+function make_tree_entry(path, string_content)
+{
+  return {
+    path: path,
+    mode: "100644",
+    content: string_content,
+    type: "blob"
+  }
+}
+
+GHAccount.prototype.make_tree_blob = function (string_content)
+{
+  let prefix = "repos/" + this.repo
+  let req = {
+    content: string_content,
+    encoding: "utf-8"
+  }
+  return this.request(prefix + "/git/blobs", "POST", JSON.stringify(req))
+}
+
+GHAccount.prototype.do_commit = function (msg, filetree, on_progress)
+{
+  let authdata = this.authdata
+  let acc = this
+  let commit_sha = ""
+  let head_json_promise = acc.getHead(function(p){on_progress(20*p)})
+  let commit_json_promise = head_json_promise.then(function(ref_json)
+    {
+      commit_sha = ref_json.object.sha
+      on_progress(20)
+      return acc.getJSON( acc.get_repo_suffix() + "git/commits/" + commit_sha, {},
+      function(p){on_progress(20*p + 20)})
+    })
+  let new_tree_promise = Promise.resolve(commit_json_promise)
+    .then(function(com_json)
+    {
+      let new_tree = {
+        base_tree: com_json.tree.sha,
+        tree: filetree
+      }
+      on_progress(40)
+      return acc.request(acc.get_repo_suffix() + "git/trees", "POST",
+        JSON.stringify(new_tree), function(p){on_progress(20*p + 40)})
+    })
+  let new_commit_promise = Promise.resolve(new_tree_promise).then(
+    function(tree_json)
+    {
+      let commit_body = {
+        message: msg,
+        tree: tree_json.sha,
+        parents: [commit_sha]
+      }
+      if (authdata.email)
+      {
+        commit_body.author = {name: authdata.uname, email: authdata.email}
+        commit_body.commiter = {name: authdata.uname, email: authdata.email}
+      }
+      on_progress(60)
+      return acc.request(acc.get_repo_suffix() + "git/commits", "POST",
+        JSON.stringify(commit_body), function(p){on_progress(20*p + 60)})
+    })
+  let updated_head_promise = Promise.resolve(new_commit_promise).then(
+    function(commit_json)
+    {
+      on_progress(80)
+      return acc.updateHead(commit_json.sha, function(p){on_progress(20*p + 80)})
+    })
+  return updated_head_promise.then(function(upd_head)
+  {
+    on_progress(100);
+    return upd_head
+  })
+}
